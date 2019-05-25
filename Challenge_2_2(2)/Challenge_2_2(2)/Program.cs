981834956f45a274a97de7de91cd2596b4cc9e7d
YuraKyziv <37220376+OtmiVi@users.noreply.github.com>
2019-05-25 16:59:20 +0300

    Add files via upload

---
+++ Challenge_2_2(2)/Challenge_2_2(2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace Challenge_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"" + Console.ReadLine();
+            while (!File.Exists(path))
+            {
+                Console.WriteLine("Enter another path");
+                path = @"" + Console.ReadLine();
+            }
+            string text = File.ReadAllText(path);
+
+            Console.Write("Enter word: ");
+            string word = Console.ReadLine();
+
+            Console.Write("Enter mode: ");
+            int mode = Int32.Parse(Console.ReadLine());
+            if (mode == 2)
+            {
+                Regex regex = new Regex($@"\W+{word}\W+", RegexOptions.IgnoreCase);
+                MatchCollection math = regex.Matches(text);
+                Console.WriteLine(math.Count.ToString());
+            }
+            else
+            {
+                Regex regex = new Regex(word, RegexOptions.IgnoreCase);
+                MatchCollection math = regex.Matches(text);
+                Console.WriteLine(math.Count.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
